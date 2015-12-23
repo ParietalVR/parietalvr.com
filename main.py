9574eaadb7b10d1438159c7a8bc9f988af57f376
Anonymous <anonymous@noemail.com>
2015-12-23 19:35:46 +0000

    Initial commit

--- main.py
+++ main.py
@@ -1,9 +1,27 @@
+import os
+from datetime import date
+import jinja2
 import webapp2
 
-class MainHandler(webapp2.RequestHandler):
+JINJA_ENVIRONMENT = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    extensions=['jinja2.ext.autoescape'],
+    autoescape=True)
+
+def prep_template_values():
+    current_year = date.today().year
+        
+    template_values = {
+        'current_year': current_year,
+    }
+    return template_values    
+
+class IndexHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello world!')
+        template_values = prep_template_values()
+        template = JINJA_ENVIRONMENT.get_template('templates/index.html')
+        self.response.write(template.render(template_values))
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+    ('/', IndexHandler)
 ], debug=True)
